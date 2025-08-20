072356be2e12ffbcc631bf36e7fe4a0fdfa4d765
DONG-GWAN KIM <gek2omg@gmail.com>
2025-08-20 17:25:57 +0900

    Qna Service 생성

--- src/main/java/com/middlepj/ict05/domain/qna/dao/QnaDao.java
+++ src/main/java/com/middlepj/ict05/domain/qna/dao/QnaDao.java
@@ -1,5 +1,64 @@
 package com.middlepj.ict05.domain.qna.dao;
 
+import java.util.List;
+
+import com.middlepj.ict05.domain.qna.dto.QnaDto;
+
+
 public class QnaDao {
 
+	// Qna 목록
+	public List<QnaDto> qnaList(int start, int end) {
+		return null;
+		
+	}
+	
+	// Qna 갯수 카운터
+	public int qnaCnt() {
+		return 0;
+	}
+	
+	// Qna 조회수 증가
+	public int plusReadCnt() {
+		
+		return 0;
+	}
+	
+	//Qna 상세 처리
+	public QnaDto getQnaDetail(int qa_id) {
+		
+		return null;
+	}
+	
+	//Qna 수정삭제 버튼 클릭 시 - 비밀번호 인증처리
+	public int password_chk(int qa_id, String password) {
+		
+		return 0;
+	}
+	
+	//Qna 수정 처리
+	public void updateBoard(QnaDto dto) {
+		
+	}
+	
+	//Qna 삭제 처리
+	public void deleteBoard(int qa_id) {
+		
+		
+	}
+	
+	//Qa 작성 처리
+	public void insertQna(QnaDto dto) {
+		
+	}
+	
+	//답변 작성 처리
+	public void insertComment(QnaDto dto) {
+		
+	}
+	
+	//댓글 목록
+	public List<QnaDto> commentList(int qa_id) {
+		return null;
+	}
 }
--- src/main/java/com/middlepj/ict05/domain/qna/service/QnaService.java
+++ src/main/java/com/middlepj/ict05/domain/qna/service/QnaService.java
@@ -1,8 +1,40 @@
 package com.middlepj.ict05.domain.qna.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.middlepj.ict05.common.Paging;
+import com.middlepj.ict05.domain.qna.dao.QnaDao;
+import com.middlepj.ict05.domain.qna.dto.QnaDto;
+
 
 @Service
 public class QnaService {
 
+	@Autowired
+	private QnaDao dao;
+	
+	public List<QnaDto> qnaList(HttpServletRequest request, HttpServletResponse response) {
+		
+		String pageNum = request.getParameter("pageNum");
+		
+		Paging paging = new Paging(pageNum);
+		
+		int total = dao.qnaCnt();
+		
+		int start = paging.getStartRow();
+		int end = paging.getEndRow();
+		
+		List<QnaDto> list = dao.qnaList(start, end);
+		
+		return null;
+	}
+
+	
+	
 }
