a94a703625554e47968b06807403539aee8d8669
Park miri <already0310@gmail.com>
2025-08-25 11:21:14 +0900

    daoo

--- src/main/java/com/middlepj/ict05/dao/FaqDAO.java
+++ src/main/java/com/middlepj/ict05/dao/FaqDAO.java
@@ -1,5 +1,18 @@
 package com.middlepj.ict05.dao;
 
-public interface faqDAO {
-
+public interface FaqDAO {
+	
+	// FAQ 목록
+	
+	// FAQ 게시글 상세페이 처리
+	
+	// FAQ 게시글 작성 처리
+	
+	// FAQ 게시글 수정 처리
+	
+	// FAQ 게시글 삭제 처리
+	
+	// FAQ 게시글 삭제 처리
+	
+	// 
 }
