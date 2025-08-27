9ed44166b72a41646e7f2cc7c0c999c955b406a2
veiboss <finalbossveigar1@gmail.com>
2025-08-27 16:43:33 +0900

    수정 완료:

--- src/main/java/com/middlepj/ict05/domain/mypage/myprofile/dao/MyProfileDAO.java
+++ src/main/java/com/middlepj/ict05/domain/mypage/myprofile/dao/MyProfileDAO.java
@@ -1,6 +1,6 @@
 package com.middlepj.ict05.domain.mypage.myprofile.dao;
 
-<<<<<<< HEAD
+
 import com.middlepj.ict05.domain.mypage.myprofile.dto.MyProfileDTO;
 
 public interface MyProfileDAO {
@@ -13,8 +13,5 @@ public interface MyProfileDAO {
 //	
 //	public int deleteMemberAction(String mbp_email);
 //	
-=======
-public interface MyProfileDAO {
 
->>>>>>> 63c501b0b05dbeb7b0c370660914e16baebbce85
 }
--- src/main/java/com/middlepj/ict05/domain/mypage/myprofile/dao/MyProfileDAOImpl.java
+++ src/main/java/com/middlepj/ict05/domain/mypage/myprofile/dao/MyProfileDAOImpl.java
@@ -1,6 +1,5 @@
 package com.middlepj.ict05.domain.mypage.myprofile.dao;
 
-<<<<<<< HEAD
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,8 +10,4 @@ public class MyProfileDAOImpl {
 	@Autowired
 	private SqlSession session;
 	
-=======
-public class MyProfileDAOImpl {
-
->>>>>>> 63c501b0b05dbeb7b0c370660914e16baebbce85
 }
