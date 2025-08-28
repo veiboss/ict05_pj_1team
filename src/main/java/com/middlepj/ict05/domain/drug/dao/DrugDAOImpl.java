ee068c3ac7cd5b7943c7f5418db147b80eb84d9a
choiminjin <chlalswls2409@naver.com>
2025-08-28 15:50:22 +0900

    목록 페이징

--- src/main/java/com/middlepj/ict05/domain/drug/dao/DrugDAOImpl.java
+++ src/main/java/com/middlepj/ict05/domain/drug/dao/DrugDAOImpl.java
@@ -1,8 +1,15 @@
 package com.middlepj.ict05.domain.drug.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.middlepj.ict05.domain.drug.dto.DrugDTO;
+import com.middlepj.ict05.domain.drug.dto.DrugReviewDTO;
+import com.middlepj.ict05.domain.drug.dto.DrugSearchDTO;
 
 @Repository
 public class DrugDAOImpl implements DrugDAO{
@@ -10,26 +17,71 @@ public class DrugDAOImpl implements DrugDAO{
 	@Autowired
 	private SqlSession sqlSession;
 
-	// 영양제 검색
-
+	// 영양제 검색 
+	@Override
+	public List<DrugSearchDTO> drugSearch(String keyword) {
+		System.out.println("=== drugDAO - drugSearch() ===");
+		
+		//List<DrugSearchDTO> list = sqlSession.selectList("", keyword);
+		
+		return null;
+	}
 
 	// 영양제 목록
-
-
+	@Override
+	public List<DrugDTO> drugList(Map<String, Object> map) {
+		System.out.println("=== drugDAO - drugList() ===");
+		
+		return null;
+	}
+	
 	// 해당 영양제 개수 카운팅
-
-
+	@Override
+	public int drugCnt() {
+		System.out.println("=== drugDAO - drugCnt() ===");
+		
+		int selectCnt = sqlSession.selectOne("com.middlepj.ict05.domain.drug.dao.DrugDAO.drugCnt");
+		
+		return selectCnt;
+	}
+	
 	// 영양제 추가 버튼 클릭 시 - 내 영양제에 추가
-
-
+	@Override
+	public int addDrug(String mbId, int drId) {
+		System.out.println("=== drugDAO - addDrug() ===");
+		
+		return 0;
+	}
+	
+	// 영양제 확인
+	@Override
+	public int countDrug(String mbId, int drId) {
+		System.out.println("=== drugDAO - countDrug() ===");
+		
+		return 0;
+	}
+	
 	// 영양제 상세 처리
-
-
+	@Override
+	public DrugDTO getDrugDetail(int dr_id) {
+		System.out.println("=== drugDAO - getDrugDetail() ===");
+		
+		return null;
+	}
+	
 	// 후기 목록
-
-
+	@Override
+	public List<DrugReviewDTO> drugReviewList(Map<String, Object> map) {
+		System.out.println("=== drugDAO - drugReviewList() ===");
+		
+		return null;
+	}
+	
 	// 후기 등록 처리
-
-
-
+	@Override
+	public void insertReview(DrugReviewDTO dto) {
+		System.out.println("=== drugDAO - insertReview() ===");
+		
+	}
+	
 }
--- src/main/java/com/middlepj/ict05/domain/drug/service/DrugServiceImpl.java
+++ src/main/java/com/middlepj/ict05/domain/drug/service/DrugServiceImpl.java
@@ -1,30 +1,123 @@
 package com.middlepj.ict05.domain.drug.service;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import com.middlepj.ict05.common.Paging;
 import com.middlepj.ict05.domain.drug.dao.DrugDAO;
+import com.middlepj.ict05.domain.drug.dto.DrugDTO;
 
 @Service
 public class DrugServiceImpl implements DrugService{
 
-	@Autowired
+	@Autowired 
 	private DrugDAO dao;
 
 	// 영양제 검색
-
+	@Override
+	public void drugSearchAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		System.out.println("=== drugService - drugSearchAction() ===");
+	
+		/*
+		 * String keyword = request.getParameter("keyword");
+		 * 
+		 * List<DrugSearchDTO> list = dao.drugSearch(keyword);
+		 * 
+		 * request.setAttribute("list", list);
+		 */
+		
+	}
 
 	// 영양제 목록
-
+	@Override
+	public void drugListAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		System.out.println("=== drugService - drugListAction() ===");
+		
+		String pageNum = request.getParameter("pageNum");
+		
+		int total = dao.drugCnt();
+		System.out.println("total : " + total);
+		
+		Paging paging = new Paging(pageNum);
+		paging.setTotalCount(total);
+		
+		int start = paging.getStartRow();
+		int end = paging.getEndRow();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<DrugDTO> list = dao.drugList(map);
+		
+		model.addAttribute("paging", paging);
+		model.addAttribute("list", list);
+	}
 
 	// 영양제 추가 클릭 시 - 내 영양제에 추가
+	@Override
+	public void drugAddAction(DrugDTO drug) {
+		System.out.println("=== drugService - drugInsertAction() ===");
+		
+		/*
+		 * String mbId = (String)session.getAttribute("mb_id"); 
+		 * member.setMb_id(mbId);
+		 * 
+		 * if(mbId == null) { 
+		 * 		return "user/login/login"; 
+		 * } 
+		 * else { 
+		 * 		int count = service.countDrug(mbId, drug.getDr_id()); 
+		 * 		if(count == 0) {
+		 * 			service.drugAddAction(drug); } else { // 있으면 alert창으로 } 
+		 * 		}
+		 * 
+		 * return "";
+		 */
+	}
+	
+	// 영양제 확인
+	@Override
+	public int countDrug(String mbId, int drId) {
 
+		return 0;
+	}
 
 	// 영양 상세 처리
-
+	@Override
+	public void drugDetailAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		System.out.println("=== drugService - drugDetailAction() ===");
+		
+	}
 
 	// 후기 목록
+	@Override
+	public void reviewListAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		System.out.println("=== drugService - reviewListAction() ===");
+		
+	}
 
+	// 후기 등록 처리 
+	@Override
+	public void reviewInsertAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		System.out.println("=== drugService - reviewAddAction() ===");
+		
+	}
 
-	// 후기 등록 처리
 }
+
