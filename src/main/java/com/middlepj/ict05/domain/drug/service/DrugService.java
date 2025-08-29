25acd2d803cb2344fce2a8d0d79594f385c1fc22
f_minjin <cmjmj5656@gmail.com>
2025-08-29 18:35:18 +0900

    목록_추가전_민진

--- src/main/java/com/middlepj/ict05/domain/drug/service/DrugService.java
+++ src/main/java/com/middlepj/ict05/domain/drug/service/DrugService.java
@@ -12,19 +12,17 @@ import com.middlepj.ict05.domain.drug.dto.DrugDTO;
 
 public interface DrugService {
 
-	// 영양제 검색 
-	public void drugSearchAction(HttpServletRequest request, HttpServletResponse response, Model model)
-			throws ServletException, IOException;
-	
 	// 영양제 목록
 	public void drugListAction(HttpServletRequest request, HttpServletResponse response, Model model)
 			throws ServletException, IOException;
 	
 	// 영양제 추가 클릭 시 - 내 영양제에 추가
-	public void drugAddAction(DrugDTO drug);
+	public void drugAddAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException;
 	
 	// 영양제 확인
-	public int countDrug(String mb_id, int dr_id);
+	public int countMyDrug(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException;
 	
 	// 영양 상세 처리
 	public void drugDetailAction(HttpServletRequest request, HttpServletResponse response, Model model)
