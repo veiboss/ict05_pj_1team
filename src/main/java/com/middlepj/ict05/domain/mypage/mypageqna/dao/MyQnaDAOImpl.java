40250f368d16f42c36de64d88fb2e7bcf8ee25ab
cosmoscenery <cosmoscenery7@gmail.com>
2025-09-01 09:26:13 +0900

    qna틀..

--- src/main/java/com/middlepj/ict05/domain/mypage/mypageqna/dao/MyQnaDAOImpl.java
+++ src/main/java/com/middlepj/ict05/domain/mypage/mypageqna/dao/MyQnaDAOImpl.java
@@ -1,5 +1,35 @@
 package com.middlepj.ict05.domain.mypage.mypageqna.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.middlepj.ict05.domain.mypage.mypageqna.dto.MyQnaDTO;
+
 public class MyQnaDAOImpl implements MyQnaDAO{
+
+	@Override
+	public List<MyQnaDTO> qnaList(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int listCnt(int mb_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	// 게시글 목록
+	
+	
+	// 게시글 갯수 구하기
+	
+	// 2. 수정버튼 클릭시 - 전문가 댓글 없을 시에 수정 페이지 이동
+	
+
+	// 3. 게시글 수정 
+	
+	
+	// 게시글 삭제 버튼 클릭시 - 전문가 댓글 없을 시에/ 삭제 (안보임처리)
 
 }
--- src/main/java/com/middlepj/ict05/domain/mypage/mypageqna/service/MyQnaServiceImpl.java
+++ src/main/java/com/middlepj/ict05/domain/mypage/mypageqna/service/MyQnaServiceImpl.java
@@ -1,5 +1,28 @@
 package com.middlepj.ict05.domain.mypage.mypageqna.service;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+
 public class MyQnaServiceImpl implements MyQnaService{
+
+	// 1. qna 상세 목록 
+	@Override
+	public void qnaListAction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws ServletException, IOException {
+		
+	}
+	
+	// 2. 수정버튼 클릭시 - 전문가 댓글 없을 시에 수정 페이지 이동
+	
+	
+	// 3. 게시글 수정 
+	
+	
+	// 게시글 삭제 버튼 클릭시 - 전문가 댓글 없을 시에/ 삭제 (안보임처리)
 
 }
