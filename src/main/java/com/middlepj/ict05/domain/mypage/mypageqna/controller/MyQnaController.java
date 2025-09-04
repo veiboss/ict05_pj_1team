3d1cf1102bdf42f7cf7d1bdb08c82f73f41ef401
ok chaeeun <cosmoscenery7@gmail.com>
2025-09-04 17:23:44 +0900

    myqna 수정, 삭제 완

--- src/main/java/com/middlepj/ict05/domain/mypage/mypageqna/controller/MyQnaController.java
+++ src/main/java/com/middlepj/ict05/domain/mypage/mypageqna/controller/MyQnaController.java
@@ -1,6 +1,7 @@
 package com.middlepj.ict05.domain.mypage.mypageqna.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.middlepj.ict05.domain.mypage.mypageqna.dto.MyQnaDTO;
 import com.middlepj.ict05.domain.mypage.mypageqna.service.MyQnaServiceImpl;
 import com.middlepj.ict05.domain.mypage.mypagereview.controller.MyReviewController;
 
@@ -42,34 +44,51 @@ private static final Logger logger = LoggerFactory.getLogger(MyReviewController.
 			 throws ServletException, IOException { 
 		 logger.info("<<< url ==> /myQnaDetail.do >>>");
 	
-		 // service.qnaDetailAction(request, response, model);
+		// 답변 여부 확인
+		 MyQnaDTO qna = service.getQnaById(Integer.parseInt(request.getParameter("qa_id")));
+		 if (qna.getQa_answer() != null && !qna.getQa_answer().isEmpty()) {
+		     // 답변이 이미 있는 경우
+		     return "myPage/myPageQna/myQnaUpdateBack";
+		     
+		 } else {
+		     // 답변이 없는 경우 → 디테일 페이지 이동
+		     model.addAttribute("dto", qna);
+		     return "myPage/myPageQna/myQnaDetail";
+		 }
+	 
+	 } 
+	
+	 // qna 수정 
+	 @RequestMapping("/myQnaUpdate.do")
+	 public String myQnaUpdate(HttpServletRequest request, HttpServletResponse response, Model model) 
+			 throws ServletException, IOException { 
+		 logger.info("<<< url ==> /myQnaUpdate.do >>>");
+	
+		 service.qnaUpdateAction(request, response, model);
 		 
 		 return "myPage/myPageQna/myQnaDetail"; 
 	 
 	 } 
 	
+	 // qna 삭제 버튼 클릭시 - 전문가 댓글 없을 시에/ 삭제 (안보임처리)
+	 @RequestMapping("/myQnaDelete.do")
+	 public String myQnaDelete(HttpServletRequest request, HttpServletResponse response, Model model) 
+			 throws ServletException, IOException { 
+		 logger.info("<<< url ==> /myQnaDelete.do >>>");
 	
-//	 // qna 수정 
-//	 @RequestMapping("/myQnaUpdate.do")
-//	 public String myQnaUpdate(HttpServletRequest request, HttpServletResponse response, Model model) 
-//			 throws ServletException, IOException { 
-//		 logger.info("<<< url ==> /myQnaUpdate.do >>>");
-//	
-//		 service.qnaUpdateAction(request, response, model);
-//		 
-//		 return "myPage/myPageQna/myQnaDetail"; 
-//	 
-//	 } 
-//	
-//	 // qna 삭제 버튼 클릭시 - 전문가 댓글 없을 시에/ 삭제 (안보임처리)
-//	 @RequestMapping("/myQnaDelete.do")
-//	 public String myQnaDelete(HttpServletRequest request, HttpServletResponse response, Model model) 
-//			 throws ServletException, IOException { 
-//		 logger.info("<<< url ==> /myQnaDelete.do >>>");
-//	
-//		 service.qnaDeleteAction(request, response, model);
-//		 
-//		 return "myPage/myPageQna/myQnaList"; 
-//	 
-//	 } 
+		 
+		// 답변 여부 확인
+		 MyQnaDTO qna = service.getQnaById(Integer.parseInt(request.getParameter("qa_id")));
+		 if (qna.getQa_answer() != null && !qna.getQa_answer().isEmpty()) {
+		     // 답변이 이미 있는 경우
+		     return "myPage/myPageQna/myQnaDeleteBack";
+		     
+		 } else {
+			 
+		     // 답변이 없는 경우 → 삭제jsp 이동
+			 service.qnaDeleteAction(request, response, model);
+		     return "myPage/myPageQna/myQnaDelete";
+		 }
+		 
+	 } 
 }
